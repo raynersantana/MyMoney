009f6aa28efdd16acd75594d8a235b09f344484a
Rayner Santana <raynersantana@plusoft.com>
2023-08-30 17:45:41 -0300

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import usePost from './usePost'
+import useGet from './useGet'
+import useDelete from './useDelete'
+
+const url = 'https://mymoney-san-default-rtdb.firebaseio.com/movimentacoes/2023-08.json'
 
 function App() {
+  const data = useGet(url)
+  const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
+  
+  const saveNew = () => {
+    post({ valor: 10 })
+  }
+  const doRemove = () => {
+    remove('https://mymoney-san-default-rtdb.firebaseio.com/movimentacoes/2023-08/-Nd1_RIK6QEyzVuMsJSW.json')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      { JSON.stringify(data) }
+      { data.loading && <p>Loading...</p> }
+      <button onClick={saveNew}>salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Remover</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
