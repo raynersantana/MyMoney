b05ea18410e68b4472ae6eb39c7601b7c2270b12
Rayner Santana <raynersantana@plusoft.com>
2023-09-08 19:25:17 -0300

    Adding Routes to my-money

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react'
-import Rest from './rest'
 import Header from './elements/Header'
-import Meses from './Meses'
-import AdicionarMes from './AdicionarMes'
-
-const baseURL = 'https://mymoney-san-default-rtdb.firebaseio.com/'
-
-const { useGet, usePost, useDelete } = Rest(baseURL)
-
-const url = 'https://mymoney-san-default-rtdb.firebaseio.com/movimentacoes/2023-08.json'
+import Home from './pages/Home/index'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Movimentacoes from './pages/Movimentacoes'
 
 function App() {
-  const data = useGet('meses')
-  // const [postData, post] = usePost('movimentacoes/2023-08')
-  // const [deleteData, remove] = useDelete()
-
-  const saveNew = () => {
-    // post({ valor: 10 })
-  }
-  const doRemove = () => {
-    // remove('movimentacoes/2023-08/-Nd1arVJCL9L-e4_91-F')
-  }
-
   return (
     <div>
       <Header />
-      <div className='container'>
-        <AdicionarMes />
-        <Meses />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact Component={Home} />
+          <Route path='/movimentacoes/:data' Component={Movimentacoes} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/pages/Home/Meses.js
+++ src/pages/Home/Meses.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Rest from './rest'
+import Rest from '../../utils/rest'
+import { Link } from 'react-router-dom'
 
 const baseURL = 'https://mymoney-san-default-rtdb.firebaseio.com/'
 const { useGet, usePost, useDelete } = Rest(baseURL)
@@ -29,7 +30,7 @@ const Meses = () => {
                             .map(mes => {
                                 return (
                                     <tr keys={mes}>
-                                        <td>{mes}</td>
+                                        <td><Link to={`/movimentacoes/${mes}`}>{mes}</Link></td>
                                         <td>{data.data[mes].previsao_entrada}</td>
                                         <td>{data.data[mes].entradas}</td>
                                         <td>{data.data[mes].previsao_saida}</td>
